461103d742effe46fb88d6fd9aae5184e17537a5
Jaap Papavoine <7790987+Jaapapa@users.noreply.github.com>
2025-08-27 11:09:12 +0200

    Wrap user interactions in React act

--- src/TweetForm.test.js
+++ src/TweetForm.test.js
@@ -1,5 +1,6 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { act } from "react";
 import "@testing-library/jest-dom";
 import TweetForm from "./TweetForm";
 
